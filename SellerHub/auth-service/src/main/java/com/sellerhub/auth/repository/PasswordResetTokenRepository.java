5fefbd186562ef1518944feea7e9e8bc111a21a0
khasu <khasuev01@gmail.com>
2025-12-03 21:47:06 +0300

    1221

--- SellerHub/auth-service/src/main/java/com/sellerhub/auth/repository/PasswordResetTokenRepository.java
+++ SellerHub/auth-service/src/main/java/com/sellerhub/auth/repository/PasswordResetTokenRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
-    void deleteByUser(User user);
+     void deleteByUser(User user);
 }
